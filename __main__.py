ce9a605378ef7424a4a60fe9ceb92932e9d39b62
David-Lor <17401854+David-Lor@users.noreply.github.com>
2025-09-18 18:20:14 +0200

    refactor(PR): main.py

--- __main__.py
+++ __main__.py
@@ -1,24 +1,3 @@
-import asyncio
+from twitterscraper.main import main
 
-from twitterscraper.modules.persistence.bootstrap import Repository
-from twitterscraper.modules.schedulers.twitterscraper import TwitterUserScraper, DeletedTweetsScraper, RecheckDeletedTweetsScraper
-from twitterscraper.modules.schedulers.archiver import TweetArchiver
-from twitterscraper.modules.schedulers.base import BaseScheduler
-from twitterscraper.settings import Settings
-
-
-async def amain():
-    settings = Settings.load_from_file()
-    print("Settings:", settings.model_dump_json(indent=2))
-
-    await Repository.setup()
-    schedulers: list[BaseScheduler] = [
-        TweetArchiver(),
-        DeletedTweetsScraper(),
-        RecheckDeletedTweetsScraper()
-    ]
-    schedulers.extend(TwitterUserScraper(user) for user in settings.scraper.users)
-    await asyncio.gather(*[sched.run() for sched in schedulers])
-
-
-asyncio.run(amain())
+main()
