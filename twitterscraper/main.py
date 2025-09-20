ef7afa723461505eb635c4fe5ca64a61e5f75dec
David-Lor <17401854+David-Lor@users.noreply.github.com>
2025-09-20 16:31:08 +0200

    feat: logger execution_id

--- twitterscraper/main.py
+++ twitterscraper/main.py
@@ -1,26 +1,31 @@
 import asyncio
 
-from twitterscraper.modules.persistence.bootstrap import Repository
-from twitterscraper.modules.schedulers.twitterscraper import TwitterUserScraper, DeletedTweetsScraper, RecheckDeletedTweetsScraper
-from twitterscraper.modules.schedulers.archiver import TweetArchiver
-from twitterscraper.modules.schedulers.base import BaseScheduler
-from twitterscraper.settings import Settings
-from twitterscraper.logger import logger, setup_loggers
+from .modules.persistence.bootstrap import Repository
+from .modules.schedulers.twitterscraper import TwitterUserScraper, DeletedTweetsScraper, RecheckDeletedTweetsScraper
+from .modules.schedulers.archiver import TweetArchiver
+from .modules.schedulers.base import BaseScheduler
+from .settings import Settings
+from .logger import logger, setup_loggers
+from .utils import get_id
 
 
 async def amain():
     settings = Settings.load_from_file()
-    setup_loggers(settings.log)
-    logger.info("Settings: " + settings.model_dump_json())
 
-    await Repository.setup()
-    schedulers: list[BaseScheduler] = [
-        TweetArchiver(),
-        DeletedTweetsScraper(),
-        RecheckDeletedTweetsScraper()
-    ]
-    schedulers.extend(TwitterUserScraper(user) for user in settings.scraper.users)
-    await asyncio.gather(*[sched.run() for sched in schedulers])
+    setup_loggers(settings.log)
+    execution_id = get_id()
+    with logger.contextualize(execution_id=execution_id):
+        logger.bind(settings=settings.model_dump(mode="json"))\
+            .info("Loaded settings")
+
+        await Repository.setup()
+        schedulers: list[BaseScheduler] = [
+            TweetArchiver(),
+            DeletedTweetsScraper(),
+            RecheckDeletedTweetsScraper()
+        ]
+        schedulers.extend(TwitterUserScraper(user) for user in settings.scraper.users)
+        await asyncio.gather(*[sched.run() for sched in schedulers])
 
 
 def main():
