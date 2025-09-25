8bf3cee9fc7d73e7e3a426d28c1bb97843551dc8
David-Lor <17401854+David-Lor@users.noreply.github.com>
2025-09-25 23:07:07 +0200

    feat: http req log

--- twitterscraper/modules/http.py
+++ twitterscraper/modules/http.py
@@ -1,9 +1,10 @@
 import random
 import asyncio
 import contextlib
-from httpx import AsyncClient
+from httpx import AsyncClient, Request, Response
 from .persistence.bootstrap import Repository
 from ..settings import HttpSettings
+from ..logger import logger
 
 __all__ = ["http_client", "AsyncClient"]
 
@@ -27,6 +28,31 @@ async def http_client(settings: HttpSettings, with_twitter_cookies: bool = False
     proxy = (random.choice(settings.proxies) or None) if settings.proxies else None
     timeout = settings.timeout
 
-    async with AsyncClient(proxy=proxy, timeout=timeout, headers=headers, **kwargs) as client:
+    async with AsyncClient(proxy=proxy, timeout=timeout, headers=headers, event_hooks=EVENT_HOOKS, **kwargs) as client:
         client.cookies = cookies
+        # noinspection PyTypeChecker
         yield client
+
+
+async def log_request(request: Request):
+    logger.bind(
+        http_req_method=request.method,
+        http_req_url=str(request.url),
+        http_req_headers=dict(request.headers),
+        http_req_body=request.content.decode(),
+    ).trace("HTTP Request")
+
+
+async def log_response(response: Response):
+    logger.bind(
+        http_req_method=response.request.method,
+        http_req_url=str(response.url),
+        http_res_status_code=response.status_code,
+        http_res_body=(await response.aread()).decode(),
+    ).trace("HTTP Response")
+
+
+EVENT_HOOKS = dict(
+    request=[log_request],
+    response=[log_response],
+)
